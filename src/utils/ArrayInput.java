7036d29d5d98e124967de3dadce4b9f3c21265e1
roshantodi09 <roshantodi40@gmail.com>
2026-07-21 02:08:45 +0530

    file renamed

---
+++ src/utils/ArrayInput.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayInput {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Arrays.toString(arrayInput(sc)));
+    }
+
+    public static int[] arrayInput(Scanner sc) {
+        System.out.println("Enter Size Of Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter " +(i+1)+ " Element: ");
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+}
